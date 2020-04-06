106d50a1d7266bf1a62d453563ff22892cf6df40
sunakshitejwani <sunakshitejwani@gmail.com>
2020-04-06 19:22:04 +0530

    Setting up E-Commerce website and using node-sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div>
-      Hello World
+     <HomePage />
     </div>
   );
 }
