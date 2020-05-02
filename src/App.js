df30cfc07e53ee8475225d75a17408f3906817d3
sunakshitejwani <sunakshitejwani@gmail.com>
2020-05-02 17:00:47 +0530

    Create SignInSignUp Page and create SignIn component + learn form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
+import HomePage from "./components/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component"; 
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
       {/* <HomePage /> */}
     </div>
